8e81dd1a29c0d0a5399eb9959de603fe934ed680
Matt Goldstine <bu77onboy@gmail.com>
2020-07-10 21:26:39 -0400

    First Walking Player

---
+++ GMTK2020-GameJam/Assets/Player.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float playerSpeed = 2f;
+    Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+         animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.W)){
+            transform.Translate(0, playerSpeed * Time.deltaTime, 0);
+            isWalking();
+        }
+        else if(Input.GetKey(KeyCode.S)){
+            transform.Translate(0, - playerSpeed * Time.deltaTime, 0);
+            isWalking();
+        }
+        else if(Input.GetKey(KeyCode.A)){
+            transform.Translate( - playerSpeed * Time.deltaTime, 0, 0);
+            isWalking();
+        }
+        else if(Input.GetKey(KeyCode.D)){
+            transform.Translate(playerSpeed * Time.deltaTime, 0, 0);
+            isWalking();
+        }
+        else {
+            isWalking(false);
+        }
+    }
+
+    void isWalking(bool val = true){
+        animator.SetBool("isWalking",val);
+    }
+}
