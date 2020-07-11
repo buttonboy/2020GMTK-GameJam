f9b61c2a63b9f42abf2e4d8be12a7adc0d1d17b9
Matt Goldstine <bu77onboy@gmail.com>
2020-07-11 16:39:44 -0400

    Start of Friend movement

---
+++ GMTK2020-GameJam/Assets/Scripts/Friend.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Friend : MonoBehaviour
+{
+    public float moveSpeed = 2;
+    public float minDistance = 3;
+    public float maxDistance = 10;
+    Vector3 kitchen;
+    float distance;
+    Vector3 currentPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        kitchen = GameObject.FindGameObjectWithTag("Kitchen").transform.position;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentPosition = transform.position;
+        distance = Vector3.Distance(currentPosition ,kitchen);
+        Debug.Log("Distance: " + distance + " Current Position: " + currentPosition  + " Kitchen Postition: " + kitchen);
+        if(Vector3.Distance(transform.position,kitchen) >= minDistance){
+            //transform.position += transform.forward * moveSpeed * Time.deltaTime;
+            transform.position = Vector3.MoveTowards(currentPosition,kitchen, moveSpeed * Time.deltaTime);
+        }
+
+        
+    }
+}
--- GMTK2020-GameJam/Assets/Scripts/Player.cs
+++ GMTK2020-GameJam/Assets/Scripts/Player.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public float playerSpeed = 2f;
+    public Rigidbody2D rb;
     Animator animator;
 
     // Start is called before the first frame update
