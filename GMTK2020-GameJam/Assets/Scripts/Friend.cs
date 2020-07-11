199870282981fc1a9a49b102015125cff4793580
Matt Goldstine <bu77onboy@gmail.com>
2020-07-11 17:23:29 -0400

    Simple Pathfinding

--- GMTK2020-GameJam/Assets/Scripts/Friend.cs
+++ GMTK2020-GameJam/Assets/Scripts/Friend.cs
@@ -5,16 +5,21 @@ using UnityEngine;
 public class Friend : MonoBehaviour
 {
     public float moveSpeed = 2;
-    public float minDistance = 3;
-    public float maxDistance = 10;
+    public float minDistance = 0;
+    public float maxDistance = 0;
     Vector3 kitchen;
+    GameObject[] doors;
     float distance;
     Vector3 currentPosition;
+    Vector3 target;
+
+    bool enteredDoor = false;
 
     // Start is called before the first frame update
     void Start()
     {
         kitchen = GameObject.FindGameObjectWithTag("Kitchen").transform.position;
+        doors = GameObject.FindGameObjectsWithTag("Door");
         
     }
 
@@ -23,12 +28,45 @@ public class Friend : MonoBehaviour
     {
         currentPosition = transform.position;
         distance = Vector3.Distance(currentPosition ,kitchen);
-        Debug.Log("Distance: " + distance + " Current Position: " + currentPosition  + " Kitchen Postition: " + kitchen);
-        if(Vector3.Distance(transform.position,kitchen) >= minDistance){
+        
+        
+        if(!enteredDoor){
+            target = FindClosestDoor();
+            if(target == currentPosition){
+                enteredDoor = true;
+                Debug.Log("Entred Door!");
+            }
+        } 
+        else {
+            target = kitchen;
+            //Debug.Log("Distance: " + distance + " Current Position: " + currentPosition  + " Target Postition: " + target);
+        }
+
+        Debug.Log("Distance: " + distance + " Current Position: " + currentPosition  + " Target Postition: " + target);
+
+        if(Vector3.Distance(transform.position,target) >= minDistance){
             //transform.position += transform.forward * moveSpeed * Time.deltaTime;
-            transform.position = Vector3.MoveTowards(currentPosition,kitchen, moveSpeed * Time.deltaTime);
+            transform.position = Vector3.MoveTowards(currentPosition,target, moveSpeed * Time.deltaTime);
         }
 
         
+    }
+
+    Vector3 FindClosestDoor(){
+        Vector3 bestDoor = new Vector3(Mathf.Infinity,Mathf.Infinity,Mathf.Infinity);
+        float distance;
+        float disToBest;
+
+        
+        foreach(GameObject door in doors){
+            distance = Vector3.Distance(currentPosition,door.transform.position);
+            disToBest = Vector3.Distance(currentPosition,bestDoor);
+            if(distance < disToBest){
+                bestDoor = door.transform.position;
+            }
+            
+        }
+
+        return bestDoor;
     }
 }
